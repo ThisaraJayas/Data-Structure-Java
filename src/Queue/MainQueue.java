94e432669d587a7fb7c8034b4ffa0dee904cecd0
Thisara <sasmithajayasinghe1@gmail.com>
2023-12-08 20:44:43 +0530

    comit

--- src/Queue/MainQueue.java
+++ src/Queue/MainQueue.java
@@ -1,4 +1,17 @@
 package Queue;
 
 public class MainQueue {
+    public static void main(String[] args){
+        QueueX que1 = new QueueX(5);
+        QueueX que2 = new QueueX(5);
+
+        que1.insert(45);
+        que1.insert(12);
+        que1.insert(75);
+        que1.insert(54);
+
+        int val =que1.remove();
+        System.out.println(val);
+
+    }
 }
