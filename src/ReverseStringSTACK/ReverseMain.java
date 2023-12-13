ec53a3fa5a3dfff3a065a66963d72d8be994b794
Thisara <sasmithajayasinghe1@gmail.com>
2023-12-13 15:41:55 +0530

    comit

--- src/ReverseStringSTACK/ReverseMain.java
+++ src/ReverseStringSTACK/ReverseMain.java
@@ -4,11 +4,12 @@ import static ReverseStringSTACK.MainStringBuilder.reverse;
 
 public class ReverseMain {
     public static void main(String[] args){
+//        The StringBuffer and StringBuilder classes are used when there is a
+//        necessity to make a lot of modifications to Strings of characters.
         StringBuffer sb = new StringBuffer("Thisara");
-
         reverse(sb);
 
-        System.out.println(sb);
+        System.out.println("Reversed Sting is : "+sb);
 
     }
 }
